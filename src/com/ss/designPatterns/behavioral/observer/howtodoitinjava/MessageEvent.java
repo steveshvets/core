52901da30ec1edd0ac5d87983ddafc7677fdcaec
steveshvets <steve.shvets@gmail.com>
2020-02-08 11:08:11 -0500

    changes

--- src/com/ss/designPatterns/behavioral/observer/howtodoitinjava/MessageEvent.java
+++ src/com/ss/designPatterns/behavioral/observer/howtodoitinjava/MessageEvent.java
@@ -4,11 +4,9 @@ immutable object so that no class can modify it’s content by mistake.
  */
 public class MessageEvent {
     final String messageContent;
-
     public MessageEvent(String m) {
         this.messageContent = m;
     }
-
     public String getMessageContent() {
         return messageContent;
     }
