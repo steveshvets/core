74a78d998f87b99456d4dee72b438d60a01b97ec
steveshvets <steve.shvets@gmail.com>
2020-02-14 15:18:16 -0500

    changes

--- src/com/ss/designPatterns/structural/adapter2/AdapterPatternTest.java
+++ src/com/ss/designPatterns/structural/adapter2/AdapterPatternTest.java
@@ -7,12 +7,12 @@ public class AdapterPatternTest {
     }
     private static void testObjectAdapter() {
         SocketAdapter sockAdapter = new SocketObjectAdapterImpl();
-        Volt v12 = getVolt(sockAdapter, 12);
+        Volt v12 = sockAdapter.get12Volt();
         System.out.println("v12 volts using Object Adapter=" + v12.getVolts());
     }
     private static void testClassAdapter() {
         SocketAdapter sockAdapter = new SocketClassAdapterImpl();
-        Volt v12 = getVolt(sockAdapter, 12);
+        Volt v12 = sockAdapter.get12Volt();
         System.out.println("v12 volts using Class Adapter=" + v12.getVolts());
     }
     private static Volt getVolt(SocketAdapter sockAdapter, int i) {
--- src/com/ss/designPatterns/structural/adapter2/Volt.java
+++ src/com/ss/designPatterns/structural/adapter2/Volt.java
@@ -2,17 +2,13 @@ package com.ss.designPatterns.structural.adapter2;
 
 public class Volt {
     private int volts;
-
     public Volt(int v) {
         this.volts = v;
     }
-
     public int getVolts() {
         return volts;
     }
-
     public void setVolts(int volts) {
         this.volts = volts;
     }
-
 }
