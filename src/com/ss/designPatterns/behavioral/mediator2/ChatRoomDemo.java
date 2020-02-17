5061223e8f43f0efbe2ead83f2d3e8376f8c4e86
steveshvets <steve.shvets@gmail.com>
2020-02-17 13:49:05 -0500

    changes

---
+++ src/com/ss/designPatterns/behavioral/mediator2/ChatRoomDemo.java
@@ -0,0 +1,22 @@
+package com.ss.designPatterns.behavioral.mediator2;
+
+public class ChatRoomDemo {
+    public static void main(String[] args) {
+        ChatRoom room = new ChatRoom();
+
+        Person john = new Person("John");
+        Person jane = new Person("Jane");
+
+        room.join(john); // no message here
+        room.join(jane);
+
+        john.say("hi room");
+        jane.say("oh, hey john");
+
+        Person simon = new Person("Simon");
+        room.join(simon);
+        simon.say("hi everyone!");
+
+        jane.privateMessage("Simon", "glad you could join us!");
+    }
+}
