7046c59c64cca882ee7687434364821096e4e393
steveshvets <steve.shvets@gmail.com>
2020-02-18 15:01:34 -0500

    changes

---
+++ src/com/ss/dataStructuresInJavaPart1/reverseLinkList/App.java
@@ -0,0 +1,19 @@
+package com.ss.dataStructuresInJavaPart1.reverseLinkList;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		List<Integer> myLinkedList = new LinkedList<>();
+
+		myLinkedList.insert(10);
+		myLinkedList.insert(2);
+		myLinkedList.insert(13);
+		myLinkedList.insert(5);
+		
+		myLinkedList.traverseList();
+		myLinkedList.reverse();
+		System.out.println();
+		myLinkedList.traverseList();
+	}
+}
