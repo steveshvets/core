6d5393d2e2fe86bac2aa0fb184c1112fba441973
steveshvets <steve.shvets@gmail.com>
2020-02-17 20:00:31 -0500

    changes

--- src/com/ss/designPatterns/structural/decorator2/CarDecorator.java
+++ src/com/ss/designPatterns/structural/decorator2/CarDecorator.java
@@ -1,5 +1,11 @@
 package com.ss.designPatterns.structural.decorator2;
 
+/*
+Decorator class implements the component interface and
+it has a HAS-A relationship with the component interface.
+The component variable should be accessible to the child decorator classes,
+so we will make this variable protected
+ */
 public class CarDecorator implements Car {
     protected Car car;
     public CarDecorator(Car c){
